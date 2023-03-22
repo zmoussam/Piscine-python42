351327c6683ec7d680835ac00803822906c16175
zmoussam <zakariamoussammi87@gmail.com>
2023-03-22 12:20:49 +0000

    add ex03

--- PiscinePython42/Day00/ex03/count.py
+++ PiscinePython42/Day00/ex03/count.py
@@ -1,1 +1,11 @@
-def text_analyzer(str):
+def text_analyzer(string):
+    upper, lower, punctuation, space = 0, 0, 0, 0
+    for i in range(len(string)):
+        if string[i].isupper():
+            upper += 1
+        elif string[i].islower():
+            lower += 1
+        elif string[i] == 
+    print("The text contains " + str(len(string)) + " character(s):")
+
+text_analyzer("hi")
