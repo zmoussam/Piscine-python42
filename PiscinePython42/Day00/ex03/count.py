b803c64d86b8453c320417b4496c6641bb566183
zmoussam <zakariamoussammi87@gmail.com>
2023-03-22 15:38:52 +0000

    add ex03

--- PiscinePython42/Day00/ex03/count.py
+++ PiscinePython42/Day00/ex03/count.py
@@ -1,11 +1,26 @@
-def text_analyzer(string):
-    upper, lower, punctuation, space = 0, 0, 0, 0
-    for i in range(len(string)):
-        if string[i].isupper():
-            upper += 1
-        elif string[i].islower():
-            lower += 1
-        elif string[i] == 
-    print("The text contains " + str(len(string)) + " character(s):")
-
-text_analyzer("hi")
+def text_analyzer(string=None):
+        ''' 
+            This function counts the number
+            of upper characters, lower characters,
+            punctuation and spaces in a given text. 
+        '''
+        if string is None or string == '':
+            string = input("What is the text to analyze? \n")
+        upper, lower, punctuation, space = 0, 0, 0, 0
+        if type(string) != str:
+            print("AssertionError: argument is not a string")
+            exit()
+        print("The text contains " + str(len(string)) + " character(s):")
+        for i in range(len(string)):
+            if string[i].isupper():
+                upper += 1
+            elif string[i].islower():
+                lower += 1
+            elif string[i] == ' ':
+                space += 1
+            elif not string[i].isdigit():
+                punctuation += 1
+        print("- " + str(upper) + " upper letter(s)")
+        print("- " + str(lower) + " lower letter(s)")
+        print("- " + str(punctuation) + " punctuation mark(s)")
+        print("- " + str(space) + " space(s)")
