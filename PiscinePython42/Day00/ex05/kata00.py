f1df5be403d57de2bc1f6694426a68c9bf6a1220
zmoussam <zakariamoussammi87@gmail.com>
2023-03-23 00:19:10 +0000

    add ex05 (kata00 and kata01)

---
+++ PiscinePython42/Day00/ex05/kata00.py
@@ -0,0 +1,3 @@
+kata = (19, 42, 21)
+
+print("the " + str(len(kata)) + " numbers are: " + str(kata)) 
---
+++ PiscinePython42/Day00/ex05/kata01.py
@@ -0,0 +1,8 @@
+kata = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+}
+
+for key in kata.keys():
+    print(key, "was created by", kata[key])
