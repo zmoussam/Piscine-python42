11d9d1c102793b79022905ad13782557b42d22f6
Zakaria Moussammi <zakariamoussammi87@gmail.com>
2023-06-23 18:34:26 +0100

    Fri Jun 23 18:34:26 +01 2023

--- PiscinePython42/Day00/ex07/filterwords.py
+++ PiscinePython42/Day00/ex07/filterwords.py
@@ -1,1 +1,11 @@
+import string
 import sys
+
+length = len(sys.argv)
+if (not length == 3) or not sys.argv[2].isdigit():
+    print("ERROR")
+    exit()
+for p in sys.argv[1].punctuation:
+    split_string = sys.argv[1].replace(p, '')
+lst = split_string.split()
+print(lst)
